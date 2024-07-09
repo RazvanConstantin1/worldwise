f4982aa6c0bc978321d7886a5d145a19a3216d2c
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-07-10 00:06:31 +0200

    used the css modules, built the pages and the routes for nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
+import Login from "./pages/Login.jsx";
 import Homepage from "./pages/Homepage.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
