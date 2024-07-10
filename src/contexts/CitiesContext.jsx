96f57eb4d0ac38ca5cf475fbd8f042a35afe27d4
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-07-10 16:10:02 +0200

    commit for revert

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState, useEffect } from "react/index.js";
+
+const BASE_URL = "http://localhost:9000/cities";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+
+        const res = await fetch(`${BASE_URL}`);
+        const data = await res.json();
+
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
