7d211f90bfdd4fc9a2d9cfc4165735aa28ba28bb
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-07-11 23:39:23 +0200

    added fake authentification, set up the context

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,67 @@
+import { useReducer } from "react/index.js";
+import { useContext } from "react/index.js";
+import { createContext } from "react/index.js";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (type.action) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+
+    default:
+      throw new Error("Unknow Action");
+  }
+}
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthenticated,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("This context was used outside the AuthProvider");
+}
+
+export { AuthProvider, useAuth };
